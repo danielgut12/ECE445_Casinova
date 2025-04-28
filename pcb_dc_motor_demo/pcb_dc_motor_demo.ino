1ef54da283c9ec3c11ea1eaddd7e6a8cadaf3027
danielgut12 <92731455+danielgut12@users.noreply.github.com>
2025-04-28 03:06:54 -0500

    Add files via upload

---
+++ pcb_dc_motor_demo/pcb_dc_motor_demo.ino
@@ -0,0 +1,27 @@
+int ejectionPinInput1 = 48;
+int ejectionPinInput2 = 21;
+int ejectionPWMPin = 47;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ejectionPinInput1, OUTPUT);
+  pinMode(ejectionPinInput2, OUTPUT);
+  pinMode(ejectionPWMPin,  OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(ejectionPWMPin, HIGH);
+  
+  digitalWrite(ejectionPinInput1,  HIGH);
+  digitalWrite(ejectionPinInput2, LOW);
+  delay(3000);
+
+  digitalWrite(ejectionPinInput1, LOW);
+  digitalWrite(ejectionPinInput2, LOW);
+  delay(3000);
+
+  digitalWrite(ejectionPinInput1,  LOW);
+  digitalWrite(ejectionPinInput2, HIGH);
+  delay(3000);
+}
